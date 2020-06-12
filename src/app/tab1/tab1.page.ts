1925e881d59bf5ee4404880a0ac7b533c12b20d4
luis <luis@futureimplementations.es>
2020-06-12 15:05:29 +0200

    [LP] NoJira sample code

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  menu = [{
+    CategoryId: 1,
+    PlateName: "bravas",
+    Price: 2.1,
+    Addon: { name: "extra sause", price: 0.30 },
+    Size: ["small", "medium", "large"],
+    Image: "https://images.unsplash.com/photo-1565599837634-134bc3aadce8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    Allergens: ["Wheat", "Peanut"]
+  }]
+
+  constructor() { }
+
+}
